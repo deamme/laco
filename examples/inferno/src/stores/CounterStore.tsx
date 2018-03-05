8e7b75fe0aba8d47baaddff504a0b6aeec9a61e9
deam <deamkh@gmail.com>
2018-03-05 19:10:57 +0100

    Update examples

--- examples/inferno/src/stores/CounterStore.tsx
+++ examples/inferno/src/stores/CounterStore.tsx
@@ -3,6 +3,13 @@ import { Store } from 'laco'
 // Creating a new store with an initial state { count: 0 }
 export const CounterStore = new Store({ count: 0 }, 'Counter')
 
+// Setting a condition to prevent count from going below 0
+CounterStore.setCondition((state) => {
+  if (state.count >= 0) {
+    return state
+  }
+})
+
 // Implementing some actions to update the store
 export const increment = () => CounterStore.set({ count: CounterStore.get().count + 1 }, 'Increment')
 export const decrement = () => CounterStore.set({ count: CounterStore.get().count - 1 }, 'Decrement')
