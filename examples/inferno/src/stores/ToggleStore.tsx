d511cb67d68b7a743a94184f544d64ca35288acb
deam <deamme@users.noreply.github.com>
2019-01-24 20:46:49 +0100

    Delete exampes

--- examples/inferno/src/stores/ToggleStore.tsx
+++
@@ -1,5 +0,0 @@
-import { Store } from 'laco'
-
-export const ToggleStore = new Store({ toggle: false })
-
-export const toggle = () => ToggleStore.set(({ toggle }) => ({ toggle: !toggle }), 'Toggle')
